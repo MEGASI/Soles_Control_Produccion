edb84cef75058770108979c67b4160c86c8d8c50
Fausto Martinez <soluciones64@hotmail.com>
2015-09-22 16:07:55 -0400

    Organizado el Menu de Herramienta y modificado la ventana del login para que cierre despues de que el usuario se loguee

--- CrtProduccion/vistas/WelcomeForm.xaml.cs
+++ CrtProduccion/vistas/WelcomeForm.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using System.Windows;
 
 
-
 namespace CrtProduccion
 {
     /// <summary>
@@ -10,133 +9,96 @@ namespace CrtProduccion
     public partial class WelcomeForm : Window
     {
 
-        Permiso sgPermiso;
         public WelcomeForm()
         {
-            InitializeComponent();
 
-
-           
-
-            if (!datamanager.probarPermiso("0", "acceso"))
-            {
-                // No tiene acceso al sistema, close
-                this.Close();
-            }
-
-            //sgPermiso = new Permiso(datamanager.IdUsuario, "0");
-
-            //  if (!sgPermiso.acceso) {
-            // No tiene acceso al sistema, close
-            //     this.Close();
-            // }
-
-
-
-        }
-
-        private void MenuItem_Click(object sender, RoutedEventArgs e)
-        {   
-          }
-        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
-        {
-          }
-        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
-        {
-            //sgPermiso.cargar(datamanager.IdUsuario, "P01");
-
-            if (datamanager.probarPermiso("P01","acceso"))
-            {
-
-                FrmUsuario U = new FrmUsuario();
-                U.ShowDialog();
-            }
-            else
-            {
-                // No tiene acceso
-                MessageBox.Show("No tiene permiso", "Seguridad", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
-
-
-
-            //    WindowInteropHelper parentHelper = new WindowInteropHelper(this);
-
-            //    WindowInteropHelper childHelper = new WindowInteropHelper(U);
-            //    Win32Native.SetParent(childHelper.Handle, parentHelper.Handle);
-            //}
-
-            //     public class Win32Native
-            //{
-
-            //    [System.Runtime.InteropServices.DllImport("user32.dll", EntryPoint = "SetParent")]
-
-            //    public extern static IntPtr SetParent(IntPtr childPtr, IntPtr parentPtr);
-
-        }
-        protected void MenuItem_Click_3(object sender, RoutedEventArgs e)
-        {
-            if (MessageBox.Show("Seguro que Desea Salir ?", "Salir", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.No)
-            {
-            }
-            else
-            {
+            // Controla el acceso al sistema.
+            segLogin dlg = new segLogin();
+            dlg.ShowDialog();
+            if (dlg.DialogResult.HasValue == false)
                 Application.Current.Shutdown();
-            }
-           }
-        private void MenuItem_Click_4(object sender, RoutedEventArgs e)
-        {
-            //sgPermiso.cargar(datamanager.IdUsuario, "P01");
-
-            if (datamanager.probarPermiso("P01","acceso"))
+            else
             {
-                FrmEditGroup FG = new FrmEditGroup();
-                // G.sgPermiso = sgPermiso;  //Creo que puedo hacer eso para pasarlo al formulario
-                FG.ShowDialog();
-            }
-            else {
-                // No tiene acceso
-                MessageBox.Show("No tiene permiso", "Seguridad", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                // Validar que el usuario logueado tiene acceso al sistema.
+                if (!datamanager.probarPermiso("0", "acceso") || dlg.DialogResult.Value == false)
+                {
+                    if (dlg.DialogResult.Value != false)
+                        MessageBox.Show("No tiene acceso al sistema.", "Seguridad", MessageBoxButton.OK, MessageBoxImage.Error);
+
+                    Close();
+                }
+
+                // Iniciar todos los componentes del menu principal
+                InitializeComponent();
+                opcionesEstado();
             }
 
-            
-        }
-        private void MenuItem_Click_5(object sender, RoutedEventArgs e)
-        {
-        }
-        private void MenuItem_Click_6(object sender, RoutedEventArgs e)
-        {
-            // Tienes que buscar el itemId de este
-            if (datamanager.probarPermiso("P0???", "acceso"))
-            {
-                FrmPG PG = new FrmPG();
-                PG.ShowDialog();
-            }
-        }
-        private void MenuItem_Click_7(object sender, RoutedEventArgs e)
-        {
-            FrmAUG FG = new FrmAUG();
-                FG.ShowDialog();
-        }
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {        
-        }
-        private void MenuItem_Click_8(object sender, RoutedEventArgs e)
-        {
-        }
-        private void MenuItem_Click_9(object sender, RoutedEventArgs e)
-        {
-            FrmPU PU = new FrmPU();
-            PU.ShowDialog();
-        }
-        private void MenuItem_Click_10(object sender, RoutedEventArgs e)
-        {
         }
 
-        private void MenuItem_Click_11(object sender, RoutedEventArgs e)
+
+        private void opcionesEstado()
         {
+            mainHerramientas.IsEnabled = datamanager.probarPermiso("H", "acceso");
+            herCambioClave.IsEnabled = datamanager.probarPermiso("H00", "acceso");
+
+            #region Heramientas de Seguridad
+            // Opcion de Herramienta - Seguridad
+            herSeguridad.IsEnabled = datamanager.probarPermiso("HS01", "acceso");
+
+            // Opciones de Seguridad
+            segUsuario.IsEnabled = datamanager.probarPermiso("HS0101", "acceso");
+            segGrupo.IsEnabled = datamanager.probarPermiso("HS0102", "acceso");
+            segAsignaGrupo.IsEnabled = datamanager.probarPermiso("HS0103", "acceso");
+            segPerfilGrupo.IsEnabled = datamanager.probarPermiso("HS0104", "acceso");
+            segPerfilUsuario.IsEnabled = datamanager.probarPermiso("HS0105", "acceso");
+            #endregion
 
         }
+
+
+        #region Opciones de Seguridad
+        private void segUsuario_Click(object sender, RoutedEventArgs e)
+        {
+            FrmUsuario dlg = new FrmUsuario();
+            dlg.ShowDialog();
+        }
+
+
+        private void segGrupo_Click(object sender, RoutedEventArgs e)
+        {
+            FrmEditGroup dlg = new FrmEditGroup();
+            dlg.ShowDialog();
+        }
+
+        private void segAsignaGrupo_Click(object sender, RoutedEventArgs e)
+        {
+            FrmAUG dlg = new FrmAUG();
+            dlg.ShowDialog();
+        }
+
+        private void segPerfilGrupo_Click(object sender, RoutedEventArgs e)
+        {
+            FrmPG dlg = new FrmPG();
+            dlg.ShowDialog();
+        }
+
+        private void segPerfilUsuario_Click(object sender, RoutedEventArgs e)
+        {
+            FrmPU dlg = new FrmPU();
+            dlg.ShowDialog();
+        }
+
+        private void mainSalir_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Seguro que Desea Salir ?", "Salir",
+                MessageBoxButton.YesNo, MessageBoxImage.Warning) != MessageBoxResult.No)
+                Close();
+        } 
+        #endregion
+
+
     }
-    }
- 
+}
+
 
---
+++ CrtProduccion/vistas/segLogin.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CrtProduccion
+{
+    
+    /// <summary>
+    /// Interaction logic for segLogin.xaml
+    /// </summary>
+    public partial class segLogin : Window
+    {
+        int intentos = 0;
+
+        public segLogin()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnAceptar_Click(object sender, RoutedEventArgs e)
+        {
+            intentos += 1;
+
+            if (datamanager.ValidarUsuario(UsuarioT.Text, passwordT.Password))
+            {
+                DialogResult = true;
+            }
+            else
+            {
+                MessageBox.Show("Usuario O Contraseña no son validas \n"+
+                                "Intento "+ intentos.ToString().Trim()+"/3",
+                "Error al inicio de Sesion", MessageBoxButton.OK, MessageBoxImage.Error);
+                if (intentos==3) DialogResult = false;
+
+            }
+        }
+
+        private void btnCancelar_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Seguro que Desea Salir ?", "Salir", MessageBoxButton.YesNo, MessageBoxImage.Warning) != MessageBoxResult.No)
+             {
+                DialogResult = false;
+                //Application.Current.Shutdown();
+            }
+        }
+    }
+}
